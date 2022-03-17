14586e6796c3a53da9ba5583849183a82704bc09
Dtsoy <dmitriytsoy123@gmail.com>
2022-03-17 14:48:35 -0700

    fetchAPI

--- dist/js/script.js
+++ dist/js/script.js
@@ -219,28 +219,28 @@ window.addEventListener('DOMContentLoaded', function() {
             form.insertAdjacentElement('afterend', statusMessage)
 
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json'); //в данном случае ненужно устоналливать заголовок так как уностанавливается автоматически(без json)
             const formData = new FormData(form);
+
             const object = {};
             formData.forEach(function(value, key) {
                 object[key] = value;
             })
-            const json = JSON.stringify(object); // in json
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+            fetch('server.php', {
+                    method: "POST",
+                    // header: { 'Content-type': 'application/json' },
+                    body: JSON.stringify(object)
+                })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
                     statusMessage.remove();
-                } else {
+                }).catch(() => {
                     showThanksModal(message.failure);
-                }
-            });
+                }).finally(() => {
+                    form.reset();
+                })
 
         });
 
