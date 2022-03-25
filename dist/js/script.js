7c46b8ba69fb70e6531a38903a465cccb4ed02a9
Dtsoy <dmitriytsoy123@gmail.com>
2022-03-25 14:15:57 -0700

    getResourceFromServer

--- dist/js/script.js
+++ dist/js/script.js
@@ -182,16 +182,12 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // new MenuCard(
-    //     "img/tabs/elite.jpg",
-    //     "elite",
-    //     'Меню “Премиум”',
-    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    //     21,
-    //     ".menu .container"
-    // ).render();
-
-
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({ img, altimg, title, descr, price }) => {
+    //             new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+    //         });
+    //     });
 
     // server post (forms)
 
@@ -203,10 +199,31 @@ window.addEventListener('DOMContentLoaded', function() {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     })
 
-    function postData(form) {
+    const postData = async(url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+
+    };
+
+    async function getResource(url) {
+        const res = await fetch(url)
+        if (!res.ok) {
+            throw new Error(`Could not fetch! ${url}, status ${res.status}`);
+        }
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault(); /// отменяет стандартное поведения в браузере
 
@@ -226,12 +243,9 @@ window.addEventListener('DOMContentLoaded', function() {
                 object[key] = value;
             })
 
-            fetch('server.php', {
-                    method: "POST",
-                    // header: { 'Content-type': 'application/json' },
-                    body: JSON.stringify(object)
-                })
-                .then(data => data.text())
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+            postData('http://localhost:3000/requests', json)
                 .then(data => {
                     console.log(data);
                     showThanksModal(message.success);
@@ -240,10 +254,9 @@ window.addEventListener('DOMContentLoaded', function() {
                     showThanksModal(message.failure);
                 }).finally(() => {
                     form.reset();
-                })
+                });
 
         });
-
     }
 
     function showThanksModal(message) {
@@ -269,8 +282,4 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000)
     }
-
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
 });
